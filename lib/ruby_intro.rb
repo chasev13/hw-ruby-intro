e1ee8e47109e2443048f76c1f6ea3de51bf692c4
EC2 Default User <ec2-user@ip-172-31-29-81.us-west-2.compute.internal>
2018-02-03 04:07:03 +0000

    improved readability

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,7 +2,7 @@
 
 # Part 1
 
-def sum arr
+def sum (arr)
   total = 0
   for i in 0..arr.length-1 do
     total += arr[i]
@@ -10,7 +10,7 @@ def sum arr
   return total
 end
 
-def max_2_sum arr
+def max_2_sum(arr)
   if arr.length == 0
     return arr.length
   end
@@ -18,18 +18,18 @@ def max_2_sum arr
     return arr[0]
   end
   arr.sort!
-  arr[-1] + arr[-2]
+  return arr[-1] + arr[-2]
 end
 
-def sum_to_n? arr, n
+def sum_to_n?(arr, n)
   if arr.length == 0
     false
   end
   if arr.length == 1
     if arr[0] == n
-      true
+      return true
     else
-      false
+      return false
     end
   end
   return arr.combination(2).any?{|a, b| a + b == n}
@@ -41,25 +41,25 @@ def hello(name)
   return "Hello, " + name
 end
 
-def starts_with_consonant? s
+def starts_with_consonant?(s)
   if s.length == 0
-    false
+    return false
   else
     letter = s[0]
-    'bcdfghjklmnpqrstvwxyz'.include?(letter.downcase)
+    return 'bcdfghjklmnpqrstvwxyz'.include?(letter.downcase())
   end
 end 
 
-def binary_multiple_of_4? s
-  int_s = s.to_i
+def binary_multiple_of_4?(s)
+  int_s = s.to_i()
   if s == '0'
-    true
+    return true
   elsif /[a-zA-Z^$3-9*]/.match(s)
-    false
+    return false
   elsif /^[10]*00$/.match(s) && int_s % 2 == 0
-    true
+    return true
   else
-    false
+    return false
   end
 
 end
